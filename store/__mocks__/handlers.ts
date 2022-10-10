2a853aad5b3cbd313d61df8cc1472ffd5c5933f1
Jon Smith <smithj@servercore.io>
2022-10-10 19:39:02 +1000

    Fix import

--- store/__mocks__/handlers.ts
+++ store/__mocks__/handlers.ts
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 
-import { API_HOST } from "store/api";
+import { API_HOST } from "lib/globals";
 
 export const handlers = [
   rest.get(`${API_HOST}/get`, (req, res, ctx) => {
